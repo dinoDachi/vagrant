2fd058f2072da45677da133cec609b568b5b20d4
David Allen <Dave@Davids-MacBook-Pro.local>
2015-01-16 14:45:40 +0000

    updated bootstrap-lamp script

--- bootstrap-lamp.sh
+++ bootstrap-lamp.sh
@@ -5,15 +5,15 @@ sudo ln -s /vagrant/* /var/www
 sudo rm /etc/apache2/sites-available/*
 sudo rm /etc/apache2/sites-enabled/*
 sudo echo "<VirtualHost *:80>
-        DocumentRoot /vagrant/$1/htdocs
+        DocumentRoot /vagrant/$1/public
 
-        <Directory /vagrant/$1/htdocs>
+        <Directory /vagrant/$1/public>
                 AllowOverride All
                 Require all granted
                 Options Indexes FollowSymLinks
         </Directory>
 </VirtualHost>" > /etc/apache2/sites-available/default.conf
-sudo mkdir -p /vagrant/$1/htdocs
-sudo echo "<h1>Hello World</h1>" > /vagrant/$1/htdocs/index.php
+sudo mkdir -p /vagrant/$1/public
+sudo echo "<h1>Vagrant has been set up correctly :)</h1>" > /vagrant/$1/public/index.php
 sudo ln -s /etc/apache2/sites-available/default.conf /etc/apache2/sites-enabled/default.conf
 sudo service apache2 restart

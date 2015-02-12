44eef89006d3161a450440101dc66a21aa14f502
David Allen <Dave@Davids-MacBook-Pro.local>
2015-02-12 02:28:32 +0000

    actually install mysql... duh

--- bootstrap-lamp.sh
+++ bootstrap-lamp.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 apt-get -y update
-apt-get -y install git php5 php5-curl php5-mcrypt php5-mysql php5-cli php5-xdebug apache2
+apt-get -y install git php5 php5-curl php5-mcrypt php5-mysql php5-cli php5-xdebug apache2 mysql-server libapache2-mod-auth-mysql
 sudo ln -s /vagrant/* /var/www
 sudo rm /etc/apache2/sites-available/*
 sudo rm /etc/apache2/sites-enabled/*

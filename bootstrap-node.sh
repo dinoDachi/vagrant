84add6125687dab4e2f4ad8ab94865e77af8c3da
David Allen <da@wearedaddy.com>
2015-07-03 20:54:23 +0100

    Update bootstrap-node.sh

--- bootstrap-node.sh
+++ bootstrap-node.sh
@@ -6,9 +6,9 @@ apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
 echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" | tee -a /etc/apt/sources.list.d/10gen.list
 apt-get -y update
 apt-get -y install mongodb-10gen
-su vagrant -l -c "curl https://raw.githubusercontent.com/creationix/nvm/v0.11.1/install.sh | bash"
-source ~/.profile
-su vagrant -l -c "nvm install 0.11.14"
-su vagrant -l -c "nvm use 0.11.14"
-su vagrant -l -c "nvm alias default 0.11.14"
+su vagrant -l -c "curl https://raw.githubusercontent.com/creationix/nvm/v0.25.4/install.sh | bash"
+source /home/vagrant/.profile
+su vagrant -l -c "nvm install stable"
+su vagrant -l -c "nvm use stable"
+su vagrant -l -c "nvm alias default stable"
 exit
